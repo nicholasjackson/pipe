6207cbc483999d6bffcc7068192fef1a2d31d280
Nic Jackson <jackson.nic@gmail.com>
2018-04-04 08:10:58 +0100

    Randomise port on connection pool test

--- providers/web/http_connectionpool_test.go
+++ providers/web/http_connectionpool_test.go
@@ -1,6 +1,8 @@
 package web
 
 import (
+	"fmt"
+	"math/rand"
 	"testing"
 
 	"github.com/matryer/is"
@@ -10,21 +12,23 @@ func TestCreatesNewConnectionFromEmptyPool(t *testing.T) {
 	is := is.New(t)
 	hp := NewHTTPConnectionPool()
 
-	c, err := hp.GetConnection("localhost", 19999)
+	port := rand.Intn(10000) + 50000 // generate random port between 50000 - 60000
+	c, err := hp.GetConnection("localhost", port)
 
-	is.NoErr(err)                         // should not have returned an error
-	is.True(c != nil)                     // should have created a new server
-	is.Equal("localhost:19999", c.Addr()) // should have set the correct address
+	is.NoErr(err)                                         // should not have returned an error
+	is.True(c != nil)                                     // should have created a new server
+	is.Equal(fmt.Sprintf("localhost:%d", port), c.Addr()) // should have set the correct address
 }
 
 func TestGetsExistingConnectionFromPool(t *testing.T) {
 	is := is.New(t)
 	hp := NewHTTPConnectionPool()
+	port := rand.Intn(10000) + 50000 // generate random port between 50000 - 60000
 
-	c1, err := hp.GetConnection("localhost", 19999)
+	c1, err := hp.GetConnection("localhost", port)
 	is.NoErr(err) // should not have returned an error
 
-	c2, err := hp.GetConnection("localhost", 19999)
+	c2, err := hp.GetConnection("localhost", port)
 	is.NoErr(err) // should not have returned an error
 
 	is.Equal(c1, c2) // connection 1 and 2 should be the same object
