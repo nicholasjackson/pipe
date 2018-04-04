23df9c24ab30742edf4671c362c8ffffb844a888
Nic Jackson <jackson.nic@gmail.com>
2018-04-04 08:08:01 +0100

    Randomise port to fix paralleisation problem in tests

--- providers/web/connection_test.go
+++ providers/web/connection_test.go
@@ -3,6 +3,8 @@ package web
 import (
 	"bytes"
 	"context"
+	"fmt"
+	"math/rand"
 	"net/http"
 	"testing"
 	"time"
@@ -10,21 +12,22 @@ import (
 	"github.com/matryer/is"
 )
 
-func setupHTTPConnection(t *testing.T) (*is.I, Connection, func()) {
+func setupHTTPConnection(t *testing.T) (*is.I, Connection, int, func()) {
 	is := is.New(t)
-	c := NewHTTPConnection("localhost", 18999)
+	port := rand.Intn(10000) + 50000 // generate random port between 50000 - 60000
+	c := NewHTTPConnection("localhost", port)
 
 	go c.ListenAndServe()
 	time.Sleep(10 * time.Millisecond)
 
-	return is, c, func() {
+	return is, c, port, func() {
 		ctx, _ := context.WithTimeout(context.Background(), 1*time.Second)
 		c.Shutdown(ctx)
 	}
 }
 
 func TestConnectionListensAtPath(t *testing.T) {
-	is, c, cleanup := setupHTTPConnection(t)
+	is, c, p, cleanup := setupHTTPConnection(t)
 	defer cleanup()
 
 	var called bool
@@ -36,14 +39,14 @@ func TestConnectionListensAtPath(t *testing.T) {
 
 	httpC := http.DefaultClient
 	httpC.Timeout = 1 * time.Second
-	_, err = httpC.Get("http://localhost:18999")
+	_, err = httpC.Get(fmt.Sprintf("http://localhost:%d", p))
 
 	is.NoErr(err)   // calling endpoint should not return an error
 	is.True(called) // should have called the handler
 }
 
 func TestConnectionNotListensToGetMethodWhenMethodPost(t *testing.T) {
-	is, c, cleanup := setupHTTPConnection(t)
+	is, c, p, cleanup := setupHTTPConnection(t)
 	defer cleanup()
 
 	var called bool
@@ -55,19 +58,19 @@ func TestConnectionNotListensToGetMethodWhenMethodPost(t *testing.T) {
 
 	httpC := http.DefaultClient
 	httpC.Timeout = 1 * time.Second
-	_, err = httpC.Post("http://localhost:18999", "text/plain", bytes.NewBuffer([]byte{}))
+	_, err = httpC.Post(fmt.Sprintf("http://localhost:%d", p), "text/plain", bytes.NewBuffer([]byte{}))
 
 	is.NoErr(err)           // calling endpoint should not return an error
 	is.Equal(false, called) // should not have called the handler
 }
 
 func TestConnectionCreatesHealthHandler(t *testing.T) {
-	is, _, cleanup := setupHTTPConnection(t)
+	is, _, p, cleanup := setupHTTPConnection(t)
 	defer cleanup()
 
 	httpC := http.DefaultClient
 	httpC.Timeout = 1 * time.Second
-	resp, err := httpC.Post("http://localhost:18999/_health", "text/plain", bytes.NewBuffer([]byte{}))
+	resp, err := httpC.Post(fmt.Sprintf("http://localhost:%d/_health", p), "text/plain", bytes.NewBuffer([]byte{}))
 
 	is.NoErr(err)                            // calling endpoint should not return an error
 	is.Equal(http.StatusOK, resp.StatusCode) // should have retuend status 200 for health
